a91960a6af646fe314f155176805238202001790
Abishua Prashanth <abishua73@gmail.com>
2021-12-20 12:08:33 -0500

    Added code to render the bubble graph

--- Main/js/app.js
+++ Main/js/app.js
@@ -9,7 +9,6 @@ d3.json("./data/samples.json").then((importedData) => {
 	 names = data.names;
      samples = data.samples;
      metadata = data.metadata;
-     console.log(samples)
 
 	names.forEach((name) => {
 		d3.select("#selDataset").append("option").text(name);
@@ -32,11 +31,11 @@ d3.json("./data/samples.json").then((importedData) => {
         let selVal  = sel.options[sel.selectedIndex].value;
 
 
-        let barGraphData = getDataForGraph(selVal);
+        let graphData = getDataForGraph(selVal, "bar");
         let barTrace = {
-            x: barGraphData[1],
-            y: barGraphData[0].map((otu_id) => `OTU ${otu_id}`),
-            text: barGraphData[2],
+            x: graphData[1],
+            y: graphData[0].map((otu_id) => `OTU ${otu_id}`),
+            text: graphData[2],
             type: 'bar',
             orientation: "h"
         }
@@ -45,23 +44,57 @@ d3.json("./data/samples.json").then((importedData) => {
 
         let layout = {
             title: "Top 10 OTUs"
-          };
+        };
           
-        Plotly.newPlot("bar", barData, layout);   
+        Plotly.newPlot("bar", barData, layout);  
+        
+        // Plot the bubble chart
+        // Use otu_ids for the x values.
+        // Use sample_values for the y values.
+        // Use sample_values for the marker size
+        // Use otu_ids for the marker colors.
+        // Use otu_labels for the text values.
+
+        let bubbleGraphData = getDataForGraph(selVal, "bubble");
+
+        let bubbleTrace = {
+            x: bubbleGraphData[0],
+            y: bubbleGraphData[1],
+            text: bubbleGraphData[2],
+            mode: 'markers',
+            marker: {
+                size: bubbleGraphData[1],
+                color: bubbleGraphData[0]
+            }
+        };
+
+        let bubbleData = [bubbleTrace];
+        let bubbleLayout = {
+            title: "Bubble Chart for the Samples"
+        };
+
+        Plotly.newPlot("bubble", bubbleData, bubbleLayout);
+
     }
 });
 
 function optionChanged(value){
 
     console.log(`Now the selected value is ${value}`);
-    // sample = getTheValuesForSelectedSample(samples, value);
-    let barGraphData = getDataForGraph(value);
+   
+    let barGraphData = getDataForGraph(value, "bar");
+    let bubbleGraphData = getDataForGraph(value, "bubble");
 
     //repolot the bar graph
     Plotly.restyle("bar", "x", [barGraphData[1]])
     Plotly.restyle("bar", "y", [barGraphData[0].map((otu_id) => `OTU ${otu_id}`)])
     Plotly.restyle("bar", "text", [barGraphData[2]])
-    console.log(sample);
+    //console.log(barGraphData);
+
+    //repolot the bubble graph
+    Plotly.restyle("bubble", "x", [bubbleGraphData[0]])
+    Plotly.restyle("bubble", "y", [bubbleGraphData[1]])
+    Plotly.restyle("bubble", "text", [bubbleGraphData[2]])
 
 }
 
@@ -73,21 +106,35 @@ function getTheValuesForSelectedSample(samples, selVal){
     }
 }
 
-function getDataForGraph(selVal){
-    let barGraphData = [];
+function getDataForGraph(selVal, top10Flag){
+    let graphData = [];
     
     //Get the selected sample data
-    sample = getTheValuesForSelectedSample(samples, selVal)
-    console.log(sample);
+    sample = getTheValuesForSelectedSample(samples, selVal);
 
+    let otu_ids = [];
+    let sample_values = [];
+    let otu_labels = [];
+    console.log(sample)
     //Select the OTU IDS for the selected value in teh drop down
-    let top_10_otu_ids = (sample.otu_ids).slice(0, 11).reverse();
-    let top_10_values = (sample.sample_values).slice(0, 11).reverse();
-    let top_10_otu_labels = (sample.otu_labels).slice(0, 11).reverse();
-    barGraphData.push(top_10_otu_ids);
-    barGraphData.push(top_10_values);
-    barGraphData.push(top_10_otu_labels);
 
-    return barGraphData;
+    if (top10Flag === "bar"){
+        console.log("Executing when the option is bar")
+        otu_ids = (sample.otu_ids).slice(0, 10).reverse();
+        sample_values = (sample.sample_values).slice(0, 10).reverse();
+        otu_labels = (sample.otu_labels).slice(0, 10).reverse();
+        
+    } else if (top10Flag === "bubble"){
+        console.log("Executing when the option is bubble")
+        otu_ids = (sample.otu_ids);
+        sample_values = (sample.sample_values);
+        otu_labels = (sample.otu_labels);
+
+    }
+    graphData.push(otu_ids);
+    graphData.push(sample_values);
+    graphData.push(otu_labels);
+    console.log(graphData)
+    return graphData;
     
 }
